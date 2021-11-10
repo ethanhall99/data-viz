ad8e8c82c13d29157f066aa405f7c59683da2f14
Ethan Hall <ethanhall99@gmail.com>
2021-11-09 22:12:50 -0600

    install package

---
+++ tidyTuesdayR.R
@@ -0,0 +1,14 @@
+# Get the Data
+
+# Read in with tidytuesdayR package 
+# Install from CRAN via: install.packages("tidytuesdayR") # select no
+# This loads the readme and all the datasets for the week of interest
+
+# Either ISO-8601 date or year/week works
+
+library(tidytuesdayR)
+
+tuesdata <- tidytuesdayR::tt_load('2021-01-05')
+tuesdata <- tidytuesdayR::tt_load(2021, week = 2)
+
+transit_cost <- tuesdata$transit_cost
